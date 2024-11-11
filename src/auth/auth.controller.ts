387a09d08a0ded58d952965b3ab2b212ea73dc39
Casintec <levyfranco@gmail.com>
2024-11-11 20:29:28 -0300

    Login system implementation. Initial phase

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { 
+  Controller, 
+  Body, 
+  Get,
+  HttpCode, 
+  HttpStatus, 
+  Post,
+  Request,
+  UseGuards 
+} from '@nestjs/common';
+
+import { AuthGuard } from './auth.guards';
+import { AuthService } from './auth.service';
+import { Public } from './constants';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Public()
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
