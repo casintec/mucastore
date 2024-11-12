73f29b2c9b37859e6853a137a2b392457a6eb31b
Casintec <levyfranco@gmail.com>
2024-11-12 15:32:11 -0300

    Implementing the user module and controller

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDto {
+  name: string
+  email: string
+  phone: string
+  cpf: string
+  password: string
+  
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,33 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-export type User = any;
-
 @Injectable()
-export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
-
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
-  }
-
+export class UserService {
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
